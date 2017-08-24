520d9e70bc037960e9d819fba3cdbaf730d4cbe2
ChuckHagy <chuckhagy@gmail.com>
2017-08-24 13:39:28 -0700

    logo added

--- src/components/InboxPageLayout.js
+++ src/components/InboxPageLayout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function InboxPageLayout(props) {
   return(
     <div>
-    <div className="boqzheader">inBOQZ<div className="titleBox"></div></div>
+    <div className="boqzheader">inB<div className="titleBox"></div>QZ</div>
       <div className="chuckMain">
         <div>{props.children[0]}</div>
         <div>{props.children[1]}</div>
